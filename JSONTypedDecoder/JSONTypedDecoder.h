7bbd6c6f15f75b0349faadd8b9fc511b099265f7
sora0077 <t.hayashi0077+github@gmail.com>
2017-04-20 16:58:08 +0900

    Initial commit

---
+++ JSONTypedDecoder/JSONTypedDecoder.h
@@ -0,0 +1,19 @@
+//
+//  JSONTypedDecoder.h
+//  JSONTypedDecoder
+//
+//  Created by 林 達也 on 2017/04/20.
+//  Copyright © 2017年 sora0077. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JSONTypedDecoder.
+FOUNDATION_EXPORT double JSONTypedDecoderVersionNumber;
+
+//! Project version string for JSONTypedDecoder.
+FOUNDATION_EXPORT const unsigned char JSONTypedDecoderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JSONTypedDecoder/PublicHeader.h>
+
+
