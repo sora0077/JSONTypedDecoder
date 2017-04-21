c8268d08813009da28e78842cb6150cc4dc178df
sora0077 <t.hayashi0077+github@gmail.com>
2017-04-21 14:37:07 +0900

    Add NSNumber decodable and fix decode

---
+++ JSONTypedDecoder/Sources/Decodable/Decodable+NSNumber.swift
@@ -0,0 +1,63 @@
+//
+//  Decodable+NSNumber.swift
+//  JSONTypedDecoder
+//
+//  Created by 林 達也 on 2017/04/21.
+//  Copyright © 2017年 sora0077. All rights reserved.
+//
+
+import Foundation
+
+extension NSNumber: Decodable {
+    public static func decode(_ decoder: Decoder) throws -> Self {
+        return try castOrFail(decoder)
+    }
+}
+
+extension Int8: Decodable {
+    public static func decode(_ decoder: Decoder) throws -> Int8 {
+        return try NSNumber.decode(decoder).int8Value
+    }
+}
+
+extension UInt8: Decodable {
+    public static func decode(_ decoder: Decoder) throws -> UInt8 {
+        return try NSNumber.decode(decoder).uint8Value
+    }
+}
+
+extension Int16: Decodable {
+    public static func decode(_ decoder: Decoder) throws -> Int16 {
+        return try NSNumber.decode(decoder).int16Value
+    }
+}
+
+extension UInt16: Decodable {
+    public static func decode(_ decoder: Decoder) throws -> UInt16 {
+        return try NSNumber.decode(decoder).uint16Value
+    }
+}
+
+extension Int32: Decodable {
+    public static func decode(_ decoder: Decoder) throws -> Int32 {
+        return try NSNumber.decode(decoder).int32Value
+    }
+}
+
+extension UInt32: Decodable {
+    public static func decode(_ decoder: Decoder) throws -> UInt32 {
+        return try NSNumber.decode(decoder).uint32Value
+    }
+}
+
+extension Int64: Decodable {
+    public static func decode(_ decoder: Decoder) throws -> Int64 {
+        return try NSNumber.decode(decoder).int64Value
+    }
+}
+
+extension UInt64: Decodable {
+    public static func decode(_ decoder: Decoder) throws -> UInt64 {
+        return try NSNumber.decode(decoder).uint64Value
+    }
+}
