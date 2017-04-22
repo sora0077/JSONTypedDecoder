f169dceb33e9c2d573e2e1dfa5f07b2062115d81
t_hayashi <t.hayashi0077+github@gmail.com>
2017-04-22 15:11:10 +0900

    Add target

---
+++ Development/Alter-macOS/Alter-macOS.h
@@ -0,0 +1,19 @@
+//
+//  Alter-macOS.h
+//  Alter-macOS
+//
+//  Created by 林達也 on 2017/04/22.
+//  Copyright © 2017年 jp.sora0077. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Alter-macOS.
+FOUNDATION_EXPORT double Alter_macOSVersionNumber;
+
+//! Project version string for Alter-macOS.
+FOUNDATION_EXPORT const unsigned char Alter_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Alter_macOS/PublicHeader.h>
+
+
--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,5 +1,5 @@
 import XCTest
-@testable import JSONTypedDecoderTests
+@testable import Alter
 
 XCTMain([
      testCase(JSONTypedDecoderTests.allTests),
