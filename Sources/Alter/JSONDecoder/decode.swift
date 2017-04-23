c883a304df9cc6683bc478aebba79dbe31c51a4d
t_hayashi <t.hayashi0077+github@gmail.com>
2017-04-24 02:42:17 +0900

    リファクタリング

--- Sources/Alter/JSONDecoder/decode.swift
+++ Sources/Alter/JSONDecoder/decode.swift
@@ -18,7 +18,7 @@ public func decode<T>(_ any: Any, rootKeyPath: KeyPath? = nil) throws -> T where
 public func decode<T>(_ any: Any, rootKeyPath: KeyPath? = nil) throws -> [T?] where T: Decodable {
     let decoder = try DecoderClass.init(any, rootKeyPath: rootKeyPath)
     guard let array = decoder.rawValue as? [Any?] else {
-        throw DecodeError.typeMismatch(expected: [Any?].self, actual: decoder.rawValue, keyPath: rootKeyPath ?? .empty)
+        throw DecodeError.typeMismatch(expected: [T?].self, actual: decoder.rawValue, keyPath: rootKeyPath ?? .empty)
     }
     return try array.map {
         do {
@@ -44,7 +44,7 @@ public func decode<T>(_ any: Any, rootKeyPath: KeyPath? = nil, skipInvalidElemen
 public func decode<T>(_ any: Any, rootKeyPath: KeyPath? = nil) throws -> [String: T?] where T: Decodable {
     let decoder = try DecoderClass.init(any, rootKeyPath: rootKeyPath ?? .empty)
     guard let dictionary = decoder.rawValue as? [String: Any?] else {
-        throw DecodeError.typeMismatch(expected: [String: Any?].self, actual: decoder.rawValue, keyPath: rootKeyPath ?? .empty)
+        throw DecodeError.typeMismatch(expected: [String: T?].self, actual: decoder.rawValue, keyPath: rootKeyPath ?? .empty)
     }
     return try dictionary.map {
         try $0.map { try DecoderClass.init($0, rootKeyPath: nil) }.map(T.decode)
